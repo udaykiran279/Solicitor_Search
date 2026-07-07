9e81538fc2539cc511a028e10427b41cb0e6ca33
Uday Kiran <udaykirannaidu19@gmail.com>
2026-07-08 00:07:53 +0100

    Update llm.py

--- llm.py
+++ llm.py
@@ -1,7 +1,6 @@
 # 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
 
-import os
-
+import json
 from groq import Groq
 from dotenv import load_dotenv
 
@@ -70,3 +69,66 @@ def ask_llm(question: str):
     )
 
     return completion.choices[0].message.content.strip()
+#print(ask_llm("my car insurance claim failed"))
+
+def classify_case(case_text: str):
+
+    prompt = f"""
+You are a legal classification engine.
+
+Your task is to identify the solicitor work areas that best match the user's legal issue.
+
+Available work areas are ONLY:
+
+Children
+Commercial / Corporate Work for Non-Listed Companies and Other
+Criminal
+Employment
+Family / Matrimonial
+Landlord and Tenant (Commercial and Domestic)
+Litigation - Other
+Non-Litigation - Other
+Personal Injury
+Probate and Estate Administration
+Property - Commercial
+Property - Residential
+Wills, Trusts and Tax Planning
+
+Rules:
+
+1. Return ONLY the work area names.
+2. Return between 1 and 3 work areas.
+3. Separate multiple work areas using commas.
+4. Do NOT use JSON.
+5. Do NOT use markdown.
+6. Do NOT use bullet points.
+7. Do NOT explain your answer.
+8. Do NOT return any text except the work area names.
+9. If no category matches, return exactly:
+Non-Litigation - Other
+
+User Case:
+{case_text}
+"""
+
+    completion = client.chat.completions.create(
+        model="openai/gpt-oss-120b",
+        temperature=0,
+        max_completion_tokens=500,
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+    print(completion)
+    response = completion.choices[0].message.content.strip()
+
+    work_areas = [
+        area.strip()
+        for area in response.split(",")
+    ]
+
+    return work_areas
+print(classify_case("car insurance claim failed"))
